ec13848efe70a748f6ded4bb8c6890a8bf3e2865
ikorihn <16367098+ikorihn@users.noreply.github.com>
2023-09-12 23:00:52 +0900

    update: linkを短縮表示しない

--- quartz.config.ts
+++ quartz.config.ts
@@ -54,7 +54,7 @@ const config: QuartzConfig = {
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.LinkCard(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest", prettyLinks: false }),
       Plugin.Description(),
     ],
     filters: [Plugin.RemoveDrafts()],
