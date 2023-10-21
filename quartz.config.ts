7545d9fa7ab0856d284d652d1f414c495922a1e8
ikorihn <16367098+ikorihn@users.noreply.github.com>
2023-10-21 15:40:47 +0900

    update: my profile

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,13 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
     enableSPA: true,
+    pageTitle: "ikorihn Digitalgarden",
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "ikorihn.github.io/digitalgarden",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
